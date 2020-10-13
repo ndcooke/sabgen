d31fe23769ca4a0050d0e910a2af776baf23449a
Michael Ivey <michael.ivey@riotgames.com>
2020-10-13 15:40:54 -0500

    more trinkets

--- chargen.js
+++ chargen.js
@@ -113,12 +113,12 @@ function charGen() {
 
     hp = Math.max(1,Math.trunc((con - 10) / 2) + roll(4));
     sp = roll(4);
-	trinket = trinkets[roll(100)];
+	trinket = trinket();
 
 
     var out = `<br>${race} ${background}, STR ${str}, DEX ${dex}, CON ${con}, INT ${int}, WIS ${wis}, CHA ${cha}, HP ${hp}, Inv: ${sp} sp, ${weapon}, ${trinket}`;
 	if (background == "Pirate") {
-		luckyCharm = trinkets[roll(100)];
+		luckyCharm = trinket();
         out += `, ${luckyCharm} (lucky charm)`;
 	}
     document.getElementById('char').innerHTML += out;
@@ -136,9 +136,13 @@ function roll3d6() {
     return x;
 }
 
+function trinket() {
+    var x = Math.floor(Math.random() * trinkets.length);
+    return trinkets[x];
+}
+
 
 var trinkets=[
-	"----",
     `a mummified goblin hand`,
     `a piece of crystal that faintly glows in the moonlight`,
     `a gold coin minted in an unknown land`,
@@ -238,5 +242,105 @@ var trinkets=[
     `a tiny mechanical crab or spider that moves about when it’s not being observed`,
     `a glass jar containing lard with a label that reads, "Griffon Grease"`,
     `a wooden box with a ceramic bottom that holds a living worm with a head on each end of its body`,
-    `a metal urn containing the ashes of a hero`
+    `a metal urn containing the ashes of a hero`,
+    `a small round coin with a hole in it made of a strange black metal`,
+    `a tiny doll depicting a blonde, blue eyes child, when placed down it's eyes appear to follow people`,
+    `a piece of cloth that absorbs any material staining it, becoming fully clean over the course of an hour`,
+    `a facsimilé of a human skull made from interwoven oak twigs`,
+    `a small charm made out of seashells strung onto a piece of leather thonging`,
+    `a whistle made from the shell of a large mollusc`,
+    `a piece of stone that changes colour based on the mood of it's owner`,
+    `the preserved skull of a corvid`,
+    `a hessian sack full of bird feathers in the most fantastic colours imaginable`,
+    `metal rank insignia from an ancient kingdom or war`,
+    `a silver thimble engraved with the image of a rose`,
+    `a reel of thread that never frays`,
+    `a metal tankard with the sign of a hammer and the initials T. E. engraved on it`,
+    `a set of bamboo windchimes`,
+    `three primitive, flint arrowheads`,
+    `an ivory comb that when used changes a person's hair colour`,
+    `a wax seal stamp with the design of a crown on it`,
+    `a stick of bright green sealing wax that smells like rosemary`,
+    `a bundle of fragrant smelling herbs that, when burnt, release a relaxing aroma`,
+    `a rough map that shows the local area but occasionally changes to show an unfamiliar continent`,
+    `a strange idol of blackened glass that sometimes glows with an inner blue light`,
+    `a flint and tinder that causes any fires light with it to be purple in colour`,
+    `a list of people's names, it gets to four people and then the fifth is smudged and crossed through`,
+    `a set of tarot cards depicting each figure on the cards as a skeleton`,
+    `a wizened, shrunken head`,
+    `a small tin containing fish eggs`,
+    `a small violin that is missing all but one of it's strings, remarkably when played it sounds as though the other strings are present`,
+    `a wolf-skin fur cloak that sometimes causes the wearer to growl under his breath`,
+    `an amulet depicting the Orc god Gruumsh, anyone who wears it finds themselves compelled to keep one of their eyes closed`,
+    `a parchment showing the constellations in the night sky`,
+    `a crystal ball, when looked into the person sees themselves reflected but looking 10 years younger`,
+    `a horseshoe that never becomes worn or needs replacing`,
+    `a tankard that is capable of changing a drink poured into another drink of similar potency (ale into stout for instance)`,
+    `a goin coin that always comes up heads when flipped`,
+    `a set of bone dice that emit an eerie cackling when rolled`,
+    `a club made out of a twisted ogres thigh bone`,
+    `a small, wooden toy horse`,
+    `a crystal that refracts the light strangely and has an odd dark deposit at it's centre`,
+    `a religious icon that once belonged to an ancestor`,
+    `a small empty book wrapped in a red velvet covering, embroidered with a the sign of an open hand`,
+    `a clothes brush that can clean a set of clothes just by touching it`,
+    `a twisting brass horn that produces a haunting melody when blow`,
+    `a map of a fictional city, anyone who sleeps near the map dreams of exploring a fantastic city`,
+    `a single rose, the colour of blood that never seems to wither or require water`,
+    `a metal chisel that never goes blunt`,
+    `a set of childs wooden building blocks, when left on their own unobserved they occasional spell out words`,
+    `three triangular platiunum coins bearing the marks of an unknown kingdom`,
+    `a pearl necklace, when there is an eclipse the pearls change from white to a black colour`,
+    `a small handkerchief that is capable of absorbing and holding four pints of fluid, it can be wrung out at any time to release the fluid`,
+    `a tin of purple powder, when the powder comes into contact with blood it glows for ten minutes`,
+    `a fake moustache, when worn it adheres to the wearer and has to be shaved off`,
+    `a set of comfortable house sleepers that keep the wearers feet at an ideal temperature`,
+    `a set of toy metal soldiers, each immaculately modelled to depict the livery of some forgotten nation`,
+    `the fire-blackened claw of some great beast`,
+    `a small stuffed vole`,
+    `the playbook from a performance where many of the audience died in a suspicious fire`,
+    `the script for a never-published play about an author going mad, the writing gets noticably less neat and more frantic, eventually stopped before the final page`,
+    `a roll of old, bandage that has been inked with strange pictograms`,
+    `a small glass jar containing a vibrant blue dye that stains the skin for many days`,
+    `a tin whistle on a chain, there is a single goose feather attached to the chain`,
+    `a small pillow stuffed with soft feathers`,
+    `a bedroll that seems to change it's colouration slightly to match the environment`,
+    `a leather pouch containing irregular iron nails, some of them bent out of shape as though used`,
+    `the pickled tentacle of an illithid`,
+    `a glass jar that appears to contain boiled cabbage, vinegar and several spices`,
+    `a tapestry depicting a mythological scene`,
+    `a set of spectacles that give everything an amber hue when worn`,
+    `a red woolen cap that appears to be stained with blood`,
+    `a set of leather riding gloves with the initials J. M. branded onto them`,
+    `a bone knife and fork`,
+    `an ivory plague mask, the beak stuffed with fragrant poseys`,
+    `an oddly shaped, small stuffed toy depicting a goblin, sometimes it seems to wink at the owner`,
+    `a quill with a small ink reserve carved into it`,
+    `a small glass orb containing sea water, it grows murky when their will be a storm at sea`,
+    `a lantern that glows black, making previously lit places dark`,
+    `an artificial nose made of copper that has a leather thong attached to it`,
+    `a wide cap made from the preserved remains of a Flumph`,
+    `a woolen scarf that is knitted with the words of an ancient elven prayer`,
+    `a lock of white drow hair`,
+    `a pamphlet for Irytor's World of the Spectacular, the phrase 'closing down' has been scrawled on it in faded ink`,
+    `a sloughed off piece of snakeskin that has words written on it in a strange language`,
+    `an apparently empty green glass bottle that is sealed with red wax`,
+    `a locket containing a picture of young halfling couple`,
+    `the stuffed and preserved head of a gnoll`,
+    `a small pot containing yellow ink, when used it is barely readable but becomes progressively darker as time passes until eventually it is black`,
+    `a series of fortune-tellers ivory tiles with letters from the common alphabet inked onto them`,
+    `a preserved eyeball with a label attached saying 'Eye of Lich.'`,
+    `a small set of pruning shears`,
+    `a glass jar containing a harmless clear gel, the jar is labelled in dwarvish as 'Gelatinous Cube – Deceased.'`,
+    `a piece of black stained parchment that pertains to be a map of the Underdark`,
+    `a brooch made of elven silver that seems to flow to fit the contours of the wearer`,
+    `a small leather pouch of pebbles that rattles audibly whenever there is an avalanche or significant other geological movement about to occur in the area`,
+    `a halfling sized set of breeches`,
+    `a wooden dresser with a secret compartment`,
+    `a metal branding iron showing the design of a dragon's wing`,
+    `a blunt carving knife with the word 'toothpick' in the language of giants carved into the handle`,
+    `a sack of Owlbear feathers`,
+    `a walking cane topped with a metal sculpture of a hydra`,
+    `a belt made from the preserved tentacle of a Displacer Beast`,
+    `an ash divining rod that twitches when water is close`
 ]
